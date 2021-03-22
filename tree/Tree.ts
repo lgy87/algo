2b331846af56cc91e330f271cd313dffb50c9953
李光耀 <lgy87@foxmail.com>
2021-03-23 02:08:56 +0800

    ooo

--- tree/Tree.ts
+++ tree/Tree.ts
@@ -6,7 +6,7 @@ type Pos = number
 
 export default class Tree<T> {
   constructor(
-    private value: TreeNode<T>,
+    private value: TreeNode<T | undefined>,
     private left?: Tree<T | undefined>,
     private right?: Tree<T | undefined>,
   ) {}
@@ -15,10 +15,21 @@ export default class Tree<T> {
 
   static of<T>(value?: T, left?: T, right?: T): Tree<T | undefined> {
     if (ra.isUndefined(value)) return Tree.empty
-
     const root = TreeNode.of(value)
     const leftChildTree = new Tree(TreeNode.of(left))
     const rightChildTree = new Tree(TreeNode.of(right))
+    console.log(
+      "of >",
+      "\n",
+      "root: ",
+      root,
+      "\n\t",
+      "leftChildTree: ",
+      leftChildTree,
+      "\n\t",
+      "rightChildTree",
+      rightChildTree,
+    )
     return new Tree(root, leftChildTree, rightChildTree)
   }
 
@@ -27,6 +38,7 @@ export default class Tree<T> {
   }
 
   private static build<T>(items: Array<T>, index: number): Tree<T | undefined> {
+    console.log("items: ", items, "index: ", index)
     if (index >= items.length) return Tree.empty
     if (ra.lengthLte(0, items)) return Tree.empty
     if (ra.lengthEq(1, items)) return Tree.of(items[0])
@@ -34,13 +46,19 @@ export default class Tree<T> {
     const root = TreeNode.of(items[index])
     const left = Tree.build(items, 2 * index + 1)
     const right = Tree.build(items, 2 * index + 2)
-
+    console.log("root: ", root, "left: ", left, "right: ", right)
     return new Tree(root, left, right)
   }
 
   height(): number {
+    console.log("--------------------height:")
     if (ra.isUndefined(this.value)) return 0
+    if (this.value === TreeNode.empty) return 0
+    console.log("this.value: ", this.value)
+    console.log("this.left: ", this.left)
+    console.log("this.right: ", this.right)
     if (this.left === Tree.empty && this.right === Tree.empty) return 0
+    if (ra.isUndefined(this.left) && ra.isUndefined(this.right)) return 0
 
     const leftTreeHeight = this.left?.height() || 0
     const rightTreeHeight = this.right?.height() || 0
@@ -58,5 +76,5 @@ export default class Tree<T> {
   }
 }
 
-const t = Tree.from([1, 2, 3, 4])
+const t = Tree.from([1])
 console.log(t.height())
